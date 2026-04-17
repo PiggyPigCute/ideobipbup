44bab21267c99d4b15d48e71d3d214440ebb79db
PiggyPigCute <piggypigcute777@gmail.com>
2026-04-17 19:37:48 +0200

    bot without commands

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 import discord
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.default(), help_command=CustomHelpCommand())
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
